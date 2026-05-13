b849c9f0bd629f588b512c1dc5fe9ff8d7f0f83f
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-05-13 02:38:29 +0000

    Tue, May 12, 2026, 10:38 PM -04:00

--- src/buttons/Mouseclick copy.jsx
+++ src/buttons/Mouseclick copy.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import useSound from 'use-sound';
-import click from '.././assets/sounds/click.wav'
-import clickimg from '.././assets/hero.png'
+import whitenoisesound from '.././assets/sounds/whitenoise.ogg'
+import whitenoiseimg from '.././assets/hero.png'
 
 function Mouseclick() {
       const [Playing, setIsPlaying] = useState(false);
-  const [play,  { stop } ] = useSound(click, {
+  const [play,  { stop } ] = useSound(whitenoisesound, {
     onplay: () => setIsPlaying(true),
   loop: true, volume: 0.5});
 
@@ -27,7 +27,7 @@ return (
 
       onClick={togglePlay}
         >
-          <img src={clickimg} alt="Click" />
+          <img src={whitenoiseimg} alt="White Noise" />
         </button>
 )
 }
